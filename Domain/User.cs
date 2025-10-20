813c3af26358886ea619092e2e7fa2f96bd8fac0
Moshe-Victor Solomon <moshevictor@yahoo.co.uk>
2025-10-21 00:28:05 +0100

    End of section 16

--- Domain/User.cs
+++ Domain/User.cs
@@ -9,7 +9,9 @@ public class User : IdentityUser
     public string? DisplayName { get; set; }
     public string? Bio { get; set; }
     public string? ImageUrl { get; set; }
-    
+
     //navigation properties
     public ICollection<ActivityAttendee> Activities { get; set; } = [];
+    public ICollection<Photo> Photos { get; set; } = [];
+    
 }
